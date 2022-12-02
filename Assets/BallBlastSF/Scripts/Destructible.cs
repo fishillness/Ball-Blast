13bf1b8f6073c107159ed865b0d7e4e37460ba55
Mariya Kolodizhanskaya <fishillness@gmail.com>
2022-12-02 15:15:18 +0700

    Scripts "Projectile" and "Destructible"
    Scripts "Projectile" and "Destructible" added.

---
+++ Assets/BallBlastSF/Scripts/Destructible.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Destructible : MonoBehaviour
+{
+    [SerializeField] private int maxHitPoints;
+
+    public UnityEvent Die;
+    public UnityEvent ChangeHitPoints;
+    private int hitPoints;
+    private void Start()
+    {
+        hitPoints = maxHitPoints;
+        ChangeHitPoints.Invoke();
+    }
+    public void ApplyDamage(int damage)
+    {
+        hitPoints -= damage;
+        ChangeHitPoints.Invoke();
+
+        if (hitPoints <= 0)
+        {
+            Kill();
+        }
+    }
+
+    public void Kill()
+    {
+        hitPoints = 0;
+        ChangeHitPoints.Invoke();
+        Die.Invoke();
+    }
+
+    public int GetHitPoints()
+    {
+        return hitPoints;
+    }
+    public int GetMaxHitPoints()
+    {
+        return maxHitPoints;
+    }
+}
---
+++ Assets/BallBlastSF/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float lifeTime;
+    [SerializeField] private int damage;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void Update()
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destructible destructible = collision.transform.root.GetComponent<Destructible>();
+        
+        if (destructible != null)
+        {
+            destructible.ApplyDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
