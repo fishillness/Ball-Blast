4ba35f911d41739bdeef3c515b894ab04683a00e
Mariya Kolodizhanskaya <fishillness@gmail.com>
2022-12-02 17:01:31 +0700

    Script "Turret"
    Script "Turret" added

--- Assets/BallBlastSF/Scripts/CartInputControl.cs
+++ Assets/BallBlastSF/Scripts/CartInputControl.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class CartInputControl : MonoBehaviour
 {
     [SerializeField] private Cart cart;
+    [SerializeField] private Turret turret;
 
     private void Update()
     {
         cart.SetMovementTarget( Camera.main.ScreenToWorldPoint(Input.mousePosition) );
+        
+        if (Input.GetMouseButton(0) == true)
+        {
+            turret.Fire();
+        }
     }
 }
---
+++ Assets/BallBlastSF/Scripts/Turret.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [SerializeField] private Projectile projectilePrefab;
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private float fireRate;
+
+    [SerializeField] private int projectileAmount;
+    [SerializeField] private float projectileInterval;
+
+    private float timer;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    private void SpawnProjectible()
+    {
+        float startPosX = shootPoint.position.x - projectileInterval * (projectileAmount - 1) * 0.5f;
+
+        for (int i = 0; i < projectileAmount; i++)
+        {
+            Instantiate(projectilePrefab, new Vector3(startPosX + i * projectileInterval, shootPoint.position.y, shootPoint.position.z), transform.rotation);
+        }
+
+    }
+
+    public void Fire()
+    {
+        if (timer >= fireRate)
+        {
+            SpawnProjectible();
+
+            timer = 0;
+        }
+    }
+}
