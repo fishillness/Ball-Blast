72bf2707e16886febec826ea73a8072b4acef4d2
Mariya Kolodizhanskaya <fishillness@gmail.com>
2022-12-02 21:38:16 +0700

    Scripts "LevelEdge", "StoneMovement"
    Scripts "LevelEdge", "StoneMovement" added

---
+++ Assets/BallBlastSF/Scripts/LevelEdge.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public enum EdgeType
+{
+    Bottom,
+    Left,
+    Right
+}
+
+public class LevelEdge : MonoBehaviour
+{
+    [SerializeField] private EdgeType type;
+
+    public EdgeType Type => type;
+
+}
